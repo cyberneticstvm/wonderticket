f49254a7ce7e4b73ff1008f3bc6fb0cac45096a0
Cybernetics <cybernetics.me@outlook.com>
2023-08-12 21:05:43 +0530

    first commit

---
+++ resources/views/admin/login.blade.php
@@ -0,0 +1,76 @@
+<!doctype html>
+<html class="no-js " lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=Edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="Wonder Ticket">
+    <meta name="keyword" content="Wonder Ticket">
+    <title>Wonder Ticket</title>
+    <link rel="icon" href="{{ asset('/administratoristrator/assets/favicon.ico') }}" type="image/x-icon"> <!-- Favicon-->
+
+    <!-- project css file  -->
+    <link rel="stylesheet" href="{{ asset('/administrator/assets/css/al.style.min.css') }}">
+    <!-- project layout css file -->
+    <link rel="stylesheet" href="{{ asset('/administrator/assets/css/layout.p.min.css') }}">
+</head>
+
+<body>
+
+<div id="layout-p" class="theme-red">
+
+    <!-- main body area -->
+    <div class="main auth-div p-2 py-3 p-xl-5">
+        
+        <!-- Body: Body -->
+        <div class="body d-flex p-0 p-xl-5">
+            <div class="container-fluid">
+
+                <div class="row g-0">
+
+                    <div class="col-lg-6 d-flex justify-content-center align-items-center border-0 rounded-lg auth-h100">
+                        <div class="w-100 p-4 p-md-5 card border-0" style="max-width: 32rem;">
+                            <!-- Form -->
+                            <form class="row g-1 p-0 p-md-4">
+                                <div class="col-12 text-center mb-5">
+                                    <h1>Sign in</h1>
+                                    <span>Free access to our dashboard.</span>
+                                </div>
+                                <div class="col-12">
+                                    <div class="mb-2">
+                                        <label class="form-label">Username</label>
+                                        <input type="text" class="form-control form-control-md" placeholder="Username">
+                                    </div>
+                                </div>
+                                <div class="col-12">
+                                    <div class="mb-2">
+                                        <div class="form-label">
+                                            <span class="d-flex justify-content-between align-items-center">
+                                                Password
+                                            </span>
+                                        </div>
+                                        <input type="password" class="form-control form-control-md" placeholder="***************">
+                                    </div>
+                                </div>
+                                <div class="col-12 text-center mt-4">
+                                    <button type="submit" class="btn btn-block btn-primary lift text-uppercase">SIGN IN</button>
+                                </div>
+                            </form>
+                            <!-- End Form -->
+                        </div>
+                    </div>
+                </div> <!-- End Row -->
+                
+            </div>
+        </div>
+
+    </div>
+
+</div>
+
+<!-- Jquery Core Js -->
+<script src="{{ asset('/administrator/assets/bundles/libscripts.bundle.js') }}"></script>
+
+</body>
+</html>
---
+++ resources/views/login.blade.php
@@ -0,0 +1,92 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    
+    <!-- Meta -->
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, minimum-scale=1, minimal-ui, viewport-fit=cover">
+	<meta name="theme-color" content="#2196f3">
+	<meta name="author" content="Cybernetics" /> 
+    <meta name="keywords" content="" /> 
+    <meta name="robots" content="" /> 
+	<meta name="description" content="Wonder Ticket"/>
+	<meta property="og:title" content="Wonder Ticket" />
+	<meta property="og:description" content="Wonder Ticket" />
+	<meta name="format-detection" content="telephone=no">
+    
+    <!-- Favicons Icon -->
+	<link rel="shortcut icon" type="image/x-icon" href="assets/images/favicon.png" />
+    
+    <!-- Title -->
+	<title>Wonder Ticket</title>
+    
+    <!-- Stylesheets -->
+    <link rel="stylesheet" href="{{ asset('/user/assets/vendor/swiper/swiper-bundle.min.css') }}">
+    <link rel="stylesheet" type="text/css" href="{{ asset('/user/assets/css/style.css') }}">
+    
+    <!-- Google Fonts -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Lato:wght@100;300;400;700;900&family=Roboto+Slab:wght@100;300;500;600;800&display=swap" rel="stylesheet">
+
+</head>   
+<body class="bg-white">
+<div class="page-wraper">
+
+    <!-- Preloader -->
+	<div id="preloader">
+		<div class="spinner"></div>
+	</div>
+    <!-- Preloader end-->
+
+    <!-- Page Content -->
+    <div class="page-content">
+        
+        <!-- Banner -->
+        <div class="banner-wrapper">
+            <div class="circle-1"></div>
+            <div class="container inner-wrapper">
+                <h1 class="dz-title">Wonder Ticket</h1>
+            </div>
+        </div>
+        <!-- Banner End -->
+        <div class="account-box">
+            <div class="container">
+                <div class="account-area">
+                    <h3 class="title">Welcome back</h3>
+					<form>
+						<div class="input-group input-mini mb-3">
+							<span class="input-group-text"><i class="fa fa-user"></i></span>
+							<input type="text" class="form-control" placeholder="Username">
+						</div>
+						<div class="mb-3 input-group input-mini">
+							<span class="input-group-text"><i class="fa fa-lock"></i></span>
+							<input type="password" class="form-control dz-password" placeholder="Password">
+							<span class="input-group-text show-pass"> 
+								<i class="fa fa-eye-slash"></i>
+								<i class="fa fa-eye"></i>
+							</span>
+						</div>
+						<div class="input-group">
+							<a href="index.html" class="btn mt-2 btn-primary w-100 btn-rounded">SIGN IN</a>
+						</div>
+						<div class="d-flex justify-content-between align-items-center">
+						</div>
+					</form>  
+                    <div class="text-center mb-auto p-tb20">
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- Page Content End -->    
+</div>
+<!--**********************************
+    Scripts
+***********************************-->
+<script src="{{ asset('/user/assets/js/jquery.js') }}"></script>
+<script src="{{ asset('/user/assets/vendor/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
+<script src="{{ asset('/user/assets/js/settings.js') }}"></script>
+<script src="{{ asset('/user/assets/js/custom.js') }}"></script>
+</body>
+</html>

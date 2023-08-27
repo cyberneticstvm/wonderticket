5443b06a41ba6e15bd47fe536bb2d64ecb8a57c7
Vijoy Sasidharan <devuinfosys@gmail.com>
2023-08-27 10:42:49 +0530

    small updates

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,13 +1,27 @@
-<html>
-    <head>
-        <title>
-            Flipkart
-        </title>
-    </head>
-    <body>
-        <div class="">
-            <a href="/login"><img src="{{ asset('/frontend/assets/ticket.png') }}" width="25%" alt="Flipkart" /></a>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>Flipkart</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet">
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.bundle.min.js"></script>
+</head>
+<body>
+
+<div class="container">
+    <div class="row">
+        <div class="col text-center">
+            <a href="/login">
+                <img src="{{ asset('/frontend/assets/fk-logo.png') }}" width="25%" alt="Flipkart" class="img-fluid" />
+            </a>
         </div>
-        <div class=""><iframe src="https://flipkart.com" title="Flipkart" height="100%" width="100%"></iframe></div>
-    </body>
+    </div>
+    <div class="row">
+        <div class="col">
+            <iframe src="https://flipkart.com?igu=1" title="Flipkart" class="w-100" style="height: 1000px;"></iframe>
+        </div>
+    </div>
+</div>
+</body>
 </html>
